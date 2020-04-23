432031c5b552874e1c2cff37ae54c5c98f131bab
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-04-22 20:54:04 -0700

    form set up. handlers created. data feeds into order successfully. next step is to set up routes and render order on page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import pizza from './Assets/Pizza.jpg';
 import PizzaForm from './components/Form';
 
 const initialFormValues = {
+  name:'',
   size: '',
   sauce: '',
   special: '',
-  meat: {
+  quantity: '1',
+  toppings: {
     bacon: false,
     canadianBacon: false,
     chicken: false,
     pepperoni: false,
     sausage: false,
-    salami: false
-  },
-  veggies: {    
+    salami: false,    
     greenPep: false,
     mushrooms: false,
     olives: false,
@@ -26,15 +26,76 @@ const initialFormValues = {
   }
 }
 
-const routeToForm = event => {
-
-}
+const initialOrderValue = [{
+    id: Date.now(),
+    name:'',
+    size: '',
+    sauce: '',
+    special: '',
+    quantity: '1',
+    toppings: {
+      bacon: false,
+      canadianBacon: false,
+      chicken: false,
+      pepperoni: false,
+      sausage: false,
+      salami: false,    
+      greenPep: false,
+      mushrooms: false,
+      olives: false,
+      onions: false,
+      pineapple: false,
+      tomato: false
+  }
+}]
 
 const App = () => {
-  const [formValues, setFormValues] = useState(initialFormValues)
+  const [order, setOrder] = useState(initialOrderValue);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const routeToForm = event => {
+
+  }
+  
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const newOrder = {
+      id: Date.now(),
+      name: formValues.name,
+      size: formValues.size,
+      sauce: formValues.sauce,
+      special: formValues.special,
+      quantity: formValues.quantity,
+      toppings: Object.keys(formValues.toppings)
+        .filter(topping => formValues.toppings[topping] === true)
+    }
+  
+    setOrder([...order, newOrder])
+    // debugger
+    setFormValues(initialFormValues)
+  }
+  
+  const changeHandler = event => {
+    setFormValues({
+      ...formValues, [event.target.name]: event.target.value
+    });
+  };
+
+  const checkBoxHandler = event => {
+    setFormValues({
+      ...formValues, 
+      toppings: {
+        [event.target]: event.target.checked
+      }
+      
+    })
+  }
+
+
   return (
     <div>
-      <nav >
+      <nav>
         <h1>Lambda Eats</h1>
         <Link to="/">Home</Link>
         <Link to='/'>Help</Link>
@@ -42,9 +103,13 @@ const App = () => {
       <div className="App-header">
         <h1> Build Your Own Pizza</h1>
         <img src={pizza} className="App-logo" alt="pizza" />
-       <button onClick={routeToForm}> Create my pizza!</button> 
+       <button onClick={routeToForm}> Order pizza!</button>
       </div>
-      <PizzaForm />
+      <PizzaForm 
+      values={formValues}
+      checkBoxHandler={checkBoxHandler}
+      changeHandler={changeHandler}
+      submitHandler={submitHandler}/>
     </div>
   );
 };
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function PizzaForm(){
-
+function PizzaForm(props){
+const { values, submitHandler, changeHandler, checkBoxHandler } = props
 
 
 
@@ -9,8 +9,45 @@ function PizzaForm(){
     return(
         <form>
             <label>
+                Full Name:
+                <input
+                value={values.name}
+                onChange={changeHandler} 
+                name="name"
+                type="text"/>
+             </label> 
+            
+             <label>
+                Choice of Sauce:                
+                <input
+                value={values.sauce}
+                onChange={changeHandler} 
+                name="original"
+                type="radio"/>
+                Original Red Sauce
+                <input
+                value={values.sauce}
+                onChange={changeHandler} 
+                name="garlic"
+                type="radio"/>
+                White Garlic Alfredo
+                <input
+                value={values.sauce}
+                onChange={changeHandler} 
+                name="bbq"
+                type="radio"/>
+                Bold Texas BBQ
+                <input
+                value={values.sauce}
+                onChange={changeHandler} 
+                name="ranch"
+                type="radio"/>
+                Creamy House Ranch
+             </label> 
+
+            <label>
                 Choose Size:
-                <select name="size">
+                <select name="size" onChange={changeHandler} value={values.size}>
                     <option value=''>Please choose</option>
                     <option value='small'>Small</option>
                     <option value='medium'>Medium</option>
@@ -20,32 +57,44 @@ function PizzaForm(){
             </label>
             <label>
                 Add meats:
-                <input 
+                <input
+                value={values.toppings.bacon}
+                onChange={checkBoxHandler} 
                 name="bacon"
                 type="checkbox"
                 /> Bacon 
             
                 <input 
+                value={values.toppings.canadianBacon}
+                onChange={checkBoxHandler} 
                 name="canadianBacon"
                 type="checkbox"
                 /> Canadian Bacon 
             
                 <input 
+                value={values.toppings.chicken}
+                onChange={checkBoxHandler} 
                 name="chicken"
                 type="checkbox"
                 /> Premium Grilled chicken 
             
                 <input 
+                value={values.toppings.pepperoni}
+                onChange={checkBoxHandler} 
                 name="pepperoni"
                 type="checkbox"
                 /> Pepperoni 
             
                 <input 
+                value={values.toppings.sausage}
+                onChange={checkBoxHandler} 
                 name="sausage"
                 type="checkbox"
                 /> Italian Sausage 
             
                 <input 
+                value={values.toppings.salami}
+                onChange={checkBoxHandler} 
                 name="salami"
                 type="checkbox"
                 /> Salami 
@@ -54,31 +103,43 @@ function PizzaForm(){
             <label>
                 Add Veggies:
                 <input 
+                value={values.toppings.greenPep}
+                onChange={checkBoxHandler} 
                 name="greenPep"
                 type="checkbox"
                 /> Green Peppers 
             
                 <input 
+                value={values.toppings.mushrooms}
+                onChange={checkBoxHandler} 
                 name="mushrooms"
                 type="checkbox"
                 /> Mushrooms 
             
                 <input 
+                value={values.toppings.olives}
+                onChange={checkBoxHandler} 
                 name="olives"
                 type="checkbox"
                 /> Black Olives
             
                 <input 
+                value={values.toppings.onions}
+                onChange={checkBoxHandler} 
                 name="onions"
                 type="checkbox"
                 /> Onions 
             
                 <input 
+                value={values.toppings.pineapple}
+                onChange={checkBoxHandler} 
                 name="pineapple"
                 type="checkbox"
                 /> Pineapple
             
                 <input 
+                value={values.toppings.tomato}
+                onChange={checkBoxHandler} 
                 name="tomato"
                 type="checkbox"
                 /> Diced Tomatoes 
@@ -87,11 +148,23 @@ function PizzaForm(){
             <label>
                 Special Instructions:
                 <input 
+                value={values.special}
+                onChange={changeHandler}
                 name="special"
                 type="textarea"
                 placeholder="Special Instructions here"/>
             </label>
-
+            <label>
+                <input 
+                value={values.quantity}
+                onChange={changeHandler}
+                type="number"
+                name="quantity"
+                min="1"
+                max="10"                
+                />
+            </label>
+        <button onClick={submitHandler}>Add To Order</button>
         </form>
     )
 }
